e10a92f9cc812ad15d83315ceaab489618392609
FelipepPrado <felipepdelimaa@gmail.com>
2024-08-30 16:20:08 -0300

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,61 +1,57 @@
 // cliente.c
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define SERVER_IP "127.0.0.1" // Endereço IP do servidor
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
     WSADATA wsaData;
-    SOCKET clientSocket;
+    SOCKET sock;
     struct sockaddr_in serverAddr;
-    int colbuffer[2];
-    char linbuffer[2];
-    int bytesReceived;
+    char buffer[BUFFER_SIZE] = {0};
+    const char *message = "Olá do cliente!";
 
-    // Inicializa o Winsock
+    // Inicializar o Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("Falha na inicialização do Winsock.\n");
+        printf("Falha na inicialização do Winsock. Código do erro: %d\n", WSAGetLastError());
         return 1;
     }
 
-    // Cria o socket do cliente
-    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (clientSocket == INVALID_SOCKET) {
-        printf("Erro ao criar o socket do cliente.\n");
+    // Criar o socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+        printf("Não foi possível criar o socket. Código do erro: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-    // Configura o endereço do servidor
+    // Configurar a estrutura do endereço do servidor
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serverAddr.sin_addr.s_addr = inet_addr("ENDERECO_IP_DO_SERVIDOR"); // Substitua pelo IP real do servidor
     serverAddr.sin_port = htons(PORT);
 
-    // Conecta ao servidor
-    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        printf("Erro ao conectar ao servidor.\n");
-        closesocket(clientSocket);
+    // Conectar ao servidor
+    if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        printf("Conexão falhou. Código do erro: %d\n", WSAGetLastError());
+        closesocket(sock);
         WSACleanup();
         return 1;
     }
 
-    scanf("%c %d",&linbuffer[0], &colbuffer[0]);
-    // Envia dados ao servidor
-    send(clientSocket, (char*)&linbuffer[0], sizeof(char), 0);
-    send(clientSocket, (char*)&colbuffer[0], sizeof(int), 0);
+    // Enviar mensagem ao servidor
+    send(sock, message, strlen(message), 0);
 
-    // Recebe resposta do servidor
-    bytesReceived = recv(clientSocket, (char*)&linbuffer[1], sizeof(char), 0);
-    recv(clientSocket, (char*)&colbuffer[1], sizeof(int), 0);
-    if (bytesReceived > 0) {
-        printf("Resposta do servidor: %c e %d\n", linbuffer[1],colbuffer[1]);
-    }
+    // Receber a resposta do servidor
+    recv(sock, buffer, BUFFER_SIZE, 0);
+    printf("Mensagem recebida do servidor: %s\n", buffer);
 
-    // Fecha o socket do cliente
-    closesocket(clientSocket);
+    // Fechar o socket
+    closesocket(sock);
     WSACleanup();
+
     return 0;
 }
