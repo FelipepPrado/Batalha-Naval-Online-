bc2fec3ead5a4248cb0249e677efe5e816ed57a7
patricpz <112330302+patricpz@users.noreply.github.com>
2024-09-19 17:59:01 -0300

    Update music_client.c

--- music_client.c
+++ music_client.c
@@ -167,20 +167,17 @@ int main() {
                 //Verifica se o jogador 2 acertou e devolve um valor de acordo com isso
                 if(realizar_ataque(mat_def,linbuffer,colbuffer)){
                     hitbuffer = 1;
-                    Sleep(6000);
                     comecar_mar();
                     printf("SEU NAVIO FOI ACERTADO\n");
                     count_dt+=1;
                 }
                 else{
                     hitbuffer = 0;
-                    Sleep(3000);
                     comecar_mar();
                     printf("%s Errou!!!!!!!\n", nick_jogador2);
                 }
                 printf("%s (Defesa)\t\t%s (Ataque)\n", nick, nick);
                 print2_mat(mat_def, mat_atk);
-                Sleep(4000);
                 comecar_mar();
 
                 //Envia para o jogador 2 se ele acertou ou não um navio
@@ -205,9 +202,8 @@ int main() {
                 scanf(" %c%d", &col, &y);
                 x = col - 'A';
                 //Realiza o som do disparo
-                const char *disparo = "C:/Users/Gustavo/Desktop/audiosbt/disparo.wav";
+                const char *disparo = "C:/Users/Patric/Documents/audiosbt/disparo.wav";
                 PlaySound(disparo, NULL, SND_FILENAME | SND_ASYNC); // Som do disparo
-                Sleep(2100);
                 while(x < 0 || x >= TAM || y < 0 || y >= TAM){
                     system("cls");
                     printf("Coordenadas Invalidas!!\n");
@@ -223,30 +219,21 @@ int main() {
                 send(clientSocket, (char*)&x, sizeof(x), 0);
                 send(clientSocket, (char*)&y, sizeof(y), 0);
 
-                const char *acertousom = "C:/Users/Gustavo/Desktop/audiosbt/acertou.wav", 
-                *errousom = "C:/Users/Gustavo/Desktop/audiosbt/errou.wav";
-
                 //Verifica se o jogador 2 ainda est� jogando e Recebe se o jogador 1 acertou ou n�o um navio
                 verificar1 = recv(clientSocket, (char*)&hitbuffer, sizeof(hitbuffer), 0);
                 if(verificar1 <= 0){
                     printf("%s desconectou-se do jogo\n\n", nick_jogador2);
                     break;
                 }
-
+                system("cls");
                 //Verifica o retorno do client se o jogador 1 acertou
                 if(hitbuffer == 1){
-                    PlaySound(acertousom, NULL, SND_FILENAME | SND_ASYNC); //Som do acerto
-                    Sleep(6000);
-                    comecar_mar();
                     mat_atk[x][y] = 'X'; // 'X' representa um navio atingido
                     printf("Acertou!!!\n");
                     count_vt+=1;
                 }
                 else{
                     if(mat_atk[x][y] == '~'){//N�o tem o 'a' na condi��o pq a matriz ataque n possui esse 'a'
-                        PlaySound(errousom, NULL, SND_FILENAME | SND_ASYNC); //Som do erro
-                        Sleep(3000);
-                        comecar_mar(); //Volta o som do mar
                         mat_atk[x][y] = 'O'; // 'O' representa um ataque na �gua
                         printf("�gua!!!\n");
                     }
@@ -254,7 +241,8 @@ int main() {
                 printf("%s (Defesa)\t\t%s (Ataque)\n", nick, nick);
                 print2_mat(mat_def, mat_atk);
                 Sleep(1500);
-                
+                comecar_mar();
+
                 //Verifica se o Jogador 1 venceu
                 if(count_vt == 3){
                     printf("%s venceu!\n\n", nick);
@@ -295,13 +283,13 @@ int main() {
 
 //Começa a música do menu
 void comecar_musica(){
-    const char *musicamenu = "C:/Users/Gustavo/Desktop/audiosbt/musicamenu.wav";
+    const char *musicamenu = "C:/Users/Patric/Documents/audiosbt/musicamenu.wav";
     PlaySound(musicamenu, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 }
 
 //Começa o som do mar
 void comecar_mar(){
-    const char *mar = "C:/Users/Gustavo/Desktop/audiosbt/mar.wav";
+    const char *mar = "C:/Users/Patric/Documents/audiosbt/mar.wav";
     PlaySound(mar, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 }
 
